25224e9cb57c367ced93c6612d0e4c137eb47bb1
yuntaeLee <leeyt1201@gmail.com>
2022-03-04 00:08:22 +0900

    Prototype Test code

--- src/test/java/hello/core/scope/PrototypeTest.java
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -1,2 +1,57 @@
-package hello.core.scope;public class PrototypeTest {
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ *  싱글톤 빈은 스프링 컨테이너 생성 시점에 초기화 메서드가 실행 되지만,
+ *  프로토타입 스코프의 빈은 컨테이너에서 빈을 조회할 때 생성되고, 초기화 메서드도 실행된다.
+ *
+ *  프로토타입 빈을 2번 조회했으므로 완전히 다른 스프링 빈이 생성되고, 초기화도 2번 실행된 것을 확인할 수 있다.
+ *
+ *  싱글톤 빈은 스프링 컨테이너가 관리하기 때문에 스프링 컨테이너가 종료될 때 빈의 종료 메서드가 실행되자만,
+ *  프로토타입 빈은 스프링 컨테이너가 생성과 의존관계 주입 그리고 초기화 까지만 관여하고, 더는 관리하지 않는다.
+ *  따라서, 프로토타입 빈은 스프링 컨테이너가 종료될 때 @PreDestory 같은 종료 메서드가 전혀 실행되지 않는다.
+ */
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+//        ac.close();
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
 }
